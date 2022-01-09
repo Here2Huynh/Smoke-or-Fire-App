79d94a33e6fab01e42056c56a2e3b148b238a3ae
John Huynh <jhnhnyh9@gmail.com>
2022-01-08 22:16:52 -0800

    chore(init): initial commit, added tailwind, adjusted web tab

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<svelte:head>
+	<title>Smoke or Fire App</title>
+</svelte:head>
+
+<div>
+	<Navbar />
+	<slot />
+</div>
+
+<style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
