313210e3a36d4489343000a36f4eb662fceec1c3
John Huynh <jhnhnyh9@gmail.com>
2022-01-11 18:43:48 -0800

    styling(player hand): adjusted hand ui to liking

--- src/components/PlayerHand.svelte
+++ src/components/PlayerHand.svelte
@@ -22,17 +22,36 @@
 <div
 	class="bg-white rounded-lg border  
 	shadow-md hover:bg-gray-100 dark:border-gray-700 
-	dark:bg-gray-800 dark:hover:bg-gray-700"
+	dark:bg-gray-800 dark:hover:bg-gray-700 p-4"
 >
-	<div class="flex flex-row">
-		<h5 class="ml-4 mt-16 text-2xl font-boldtext-gray-900 dark:text-white">
+	<div class="grid grid-cols-5 gap-0">
+		<h5 class="mt-auto mb-auto text-2xl font-boldtext-gray-900 dark:text-white">
 			{player.name}
 		</h5>
-		<div class="w-28 flex flex-row">
-			<img src="https://deckofcardsapi.com/static/img/AS.svg" alt="" width="100%" height="150%" />
-			<img src="https://deckofcardsapi.com/static/img/KS.svg" />
-			<img src="https://deckofcardsapi.com/static/img/QS.svg" />
-			<img src="https://deckofcardsapi.com/static/img/JS.svg" />
+		<!-- <div class="col-start-3 col-end-4"> -->
+		<div class="w-full h-full">
+			<img src="https://deckofcardsapi.com/static/img/AS.png" alt="" />
 		</div>
+		<div class="w-full h-full">
+			<img src="https://deckofcardsapi.com/static/img/AS.png" alt="" />
+		</div>
+		<div class="w-full h-full">
+			<img src="https://deckofcardsapi.com/static/img/AS.png" alt="" />
+		</div>
+		<div class="w-full h-full">
+			<img src="https://deckofcardsapi.com/static/img/AS.png" alt="" />
+		</div>
+
+		<!-- <img src="https://deckofcardsapi.com/static/img/KS.svg" />
+		<img src="https://deckofcardsapi.com/static/img/QS.svg" />
+		<img src="https://deckofcardsapi.com/static/img/JS.svg" /> -->
+		<!-- </div> -->
 	</div>
 </div>
+
+<style>
+	/* img {
+		width: 100px;
+		height: 200px;
+	} */
+</style>
